ee71fa094ec3c5a9d54c71dd3a5be198a5484842
James Moseley <james.wei.moseley@gmail.com>
2022-01-16 17:39:03 -0800

    added tailwind

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 function HomeScreen(props) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Text>Are you a:</Text>
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <Text style={tw`text-2xl pt-10`}>Home Screen</Text>
+      <Text style={tw`text-2xl p-10`}>Are you a:</Text>
       <Button
         onPress={() => props.navigation.navigate("Restaurant")}
         title="Restaurant"
