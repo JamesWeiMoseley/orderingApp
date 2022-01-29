7ba85269ed0c97c5b1382259b419a8c4f1f2ffb9
grierm <grierm@oregonstate.edu>
2022-01-28 16:18:11 -0800

    Updated lunch menu is working

--- src/screens/Customer/ViewRes.js
+++ src/screens/Customer/ViewRes.js
@@ -1,28 +1,84 @@
 import React from "react";
-import { Text, View, Button, FlatList, TouchableOpacity, } from "react-native";
+import { Text, View, Button, FlatList, TouchableOpacity, ListItem } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import Data from "../../dummyData.json";
 import LunchData from "./lunchData.json";
+import NewLunch from "./NewLunch"
 
-const Item = ({ title, price }) => (
+const Item = ({ lunch, price }) => (
   <View style={tw`p-5 border-solid border-2`}>
-    <Text style={tw`text-4xl text-blue-500`}>{title}</Text>
+    <Text style={tw`text-4xl text-blue-500`}>{lunch}</Text>
     <Text style={tw`text-2xl`}>{price}</Text>
   </View>
 );
 
-
 const ViewRes = (props) => {
-  console.log(props.route.params);
+  { console.log(props) }
+  { console.log(props?.route.params) }
+
+  function showLunch() {
+
+    return (
+      <View>
+        {
+          props?.route.params.lunch.map((item, index) =>
+            <View
+              key={`lunch-${index}`}
+            >
+              <View>
+                <Text>
+                  {item} {' '} {props?.route.params.price[item]}
+                </Text>
+              </View>
+            </View>
+          )
+        }
+      </View>
+    )
+  }
   return (
+
     <View style={tw`p-10`}>
+
       <Text style={tw`text-red-500 text-2xl`}>{props.route.params.title}</Text>
       <Text>Type: {props.route.params.type}</Text>
 
-        // Lunch Menu
+
       <Text style={tw`text-red-500 text-2xl`}>Lunch Menu</Text>
+      {showLunch()}
       <FlatList
-          data={LunchData}
+        data={Data}
+        renderItem={({ item }) => {
+          return (
+            <FlatList
+              data={item}
+              renderItem={({ item }) => {
+                return (
+                  <Item title={item} type={item} />
+                )
+              }}
+            ></FlatList>
+
+            // <TouchableOpacity
+            //   onPress={() => props.navigation.navigate("View", item)}
+            // >
+            //   <Item title={item.lunch} price={item.price} />
+            // </TouchableOpacity>
+          );
+        }}
+        keyExtractor={(item) => item.id}
+      ></FlatList>
+
+
+    </View>
+  );
+};
+
+export default ViewRes;
+
+
+{/* <FlatList
+          data={Data}
           renderItem={({ item }) => {
             return (
               <TouchableOpacity
@@ -33,9 +89,4 @@ const ViewRes = (props) => {
             );
           }}
           keyExtractor={(item) => item.id}
-        ></FlatList>
-    </View>
-  );
-};
-
-export default ViewRes;
+        ></FlatList> */}
