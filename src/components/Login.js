a504bad597b1ce4f01a72748b924ef051198de58
James Moseley <james.wei.moseley@gmail.com>
2022-01-28 19:09:27 -0800

    added sign up and verification

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Button, TextInput } from "react-native";
+import { View, Text, Button, TextInput, Alert } from "react-native";
 import { Auth, Hub } from "aws-amplify";
 import tw from "tailwind-react-native-classnames";
 
@@ -15,7 +15,8 @@ function Login(props) {
       setPassword("");
       props.navigation.navigate("Portal");
     } catch (error) {
-      console.log("error signing in", error);
+      Alert.alert("One or more of the fields is missing");
+      console.log("error signing in");
     }
   }
 
@@ -41,6 +42,12 @@ function Login(props) {
         <Button
           onPress={() => props.navigation.navigate("Signup")}
           title="Signup"
+        ></Button>
+      </View>
+      <View>
+        <Button
+          onPress={() => props.navigation.navigate("Verify")}
+          title="Verify an Account"
         ></Button>
       </View>
     </View>
