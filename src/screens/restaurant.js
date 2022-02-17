c8a96a39a1a4b8671a4926c2626a1058f9cd4ffa
James Moseley <james.wei.moseley@gmail.com>
2022-02-16 23:40:39 -0800

    now can get post to database

--- src/screens/restaurant.js
+++ src/screens/restaurant.js
@@ -1,18 +1,82 @@
-import React, { useEffect } from "react";
-import { View, Text, Button, TextInput } from "react-native";
+import { API, graphqlOperation } from "aws-amplify";
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, TextInput, FlatList } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import { listRestaurants } from "../graphql/queries";
+import * as mutations from "../graphql/mutations";
 
 function Restaurant() {
+  const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const postsResult = await API.graphql(
+          graphqlOperation(listRestaurants)
+        );
+        setPosts(postsResult.data.listRestaurants.items);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  const Item = ({ title, type }) => (
+    <View style={tw`p-5 border-solid border-2`}>
+      <Text style={tw`text-3xl text-blue-500`}>{title}</Text>
+      <Text style={tw`text-2xl`}>{type}</Text>
+    </View>
+  );
+
+  const RestaurantPost = {
+    title: title,
+    type: type,
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const newRestaurant = await API.graphql({
+        query: mutations.createRestaurants,
+        variables: { input: RestaurantPost },
+      });
+      console.log(`${title} and ${type}`);
+      setTitle("");
+      setType("");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <View style={tw`bg-gray-400 h-full`}>
-      <Text style={tw`text-center pt-10`}>Sign in Below</Text>
-      <View style={tw`p-10`}>
-        <Text>Username</Text>
-        <TextInput style={tw`border-solid border-2 text-2xl`}></TextInput>
-        <Text>Password</Text>
-        <TextInput style={tw`border-solid border-2 text-2xl`}></TextInput>
+      <View style={tw`mb-5`}>
+        <Text style={tw`text-center pt-10`}>Add Restaurant</Text>
+        <View style={tw`p-10`}>
+          <Text>Title</Text>
+          <TextInput
+            onChangeText={(e) => setTitle(e)}
+            value={title}
+            style={tw`border-solid border-2 text-2xl`}
+          ></TextInput>
+          <Text>Type</Text>
+          <TextInput
+            value={type}
+            onChangeText={(type) => setType(type)}
+            style={tw`border-solid border-2 text-2xl`}
+          ></TextInput>
+        </View>
+        <Button title="Add new Restaurant" onPress={handleSubmit} />
       </View>
-      <Button title="Sign up" />
+      <Text style={tw`text-4xl`}>List of restaurants</Text>
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => {
+          return <Item title={item.title} type={item.type} />;
+        }}
+      ></FlatList>
     </View>
   );
 }
