2e6a838ad0548f96d566dca94ad05754d833cdbe
James Moseley <james.wei.moseley@gmail.com>
2022-02-22 13:39:35 -0800

    updated schema

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -5,8 +5,16 @@ export const getRestaurants = /* GraphQL */ `
   query GetRestaurants($id: ID!) {
     getRestaurants(id: $id) {
       id
+      username
       title
       type
+      items {
+        id
+        food
+        price
+        createdAt
+        updatedAt
+      }
       createdAt
       updatedAt
     }
@@ -21,8 +29,45 @@ export const listRestaurants = /* GraphQL */ `
     listRestaurants(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
+        username
         title
         type
+        items {
+          id
+          food
+          price
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getItem = /* GraphQL */ `
+  query GetItem($id: ID!) {
+    getItem(id: $id) {
+      id
+      food
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listItems = /* GraphQL */ `
+  query ListItems(
+    $filter: ModelItemFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listItems(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        food
+        price
         createdAt
         updatedAt
       }
